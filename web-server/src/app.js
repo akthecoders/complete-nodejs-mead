3753b3772a265b2284b87f667cb38efd75947eef
Akshay Kumar <akthecoder@gmail.com>
2020-01-02 15:58:57 +0530

    Lec[44]: Serving up HTML and JSON

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,19 +3,25 @@ const express = require('express');
 const app = express();
 
 app.get('', (req, res) => {
-  res.send('Hello Express!');
+  res.send('<h1>Weather</h1>');
 });
 
 app.get('/help', (req, res) => {
-  res.send('Help page');
+  res.send({
+    name: "Akshay",
+    age: 25
+  });
 });
 
 app.get('/about', (req, res) => {
-  res.send('About page');
+  res.send('<h1>About page</h1>');
 });
 
 app.get('/weather', (req, res) => {
-  res.send('Weather Forecast');
+  res.send({
+    forecast: "Forecast",
+    location: "Noida",
+  });
 });
 
 app.listen(3000, () => {
