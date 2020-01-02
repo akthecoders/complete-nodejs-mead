704ffbf754f323828687b4ab62a2de75561806ae
Akshay Kumar <akthecoder@gmail.com>
2020-01-02 13:25:58 +0530

    Lec[38]: Callback chaining

--- weather-app/app.js
+++ weather-app/app.js
@@ -2,8 +2,22 @@ const request = require('request');
 const geoCode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
-
-geoCode('Noida', (error, data) => {
-  console.log(error);
-  console.log(data);
+const location = process.argv[2];
+if(!location) {
+  console.log('Please provide location');
+  return;
+}
+geoCode(location, (error, geoData) => {
+  if(error) {
+    return console.log(error);
+  }
+  else {
+    forecast(geoData.latitude, geoData.longitude, (error, forecastData) => {
+      if(error) {
+        return console.log(error);
+      }
+      console.log(geoData.location);
+      console.log(forecastData);
+    });
+  }
 });
