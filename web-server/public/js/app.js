52f4a764fad3b6d8dd26ce973200f473cfbbcf30
Akshay Kumar <akthecoder@gmail.com>
2020-01-03 13:23:48 +0530

    Lec[59]: Wrap up the UI

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,17 +1,24 @@
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
+const messageOne = document.getElementById('message-1');
+const messageTwo = document.getElementById('message-2');
+
+messageOne.textContent = "";
+messageTwo.textContent = "";
 
 weatherForm.addEventListener('submit', (e) => {
   e.preventDefault();
   const location = search.value;
+  messageOne.textContent = "Loading...";
+  messageTwo.textContent = "";
   fetch('http://localhost:3000/weather?address=' + location).then((response) => {
     response.json(response).then((data) => {
       if(data.error) {
-        console.log(data.error);
+        messageOne.textContent = data.error;
       }
       else {
-        console.log(data.location);
-        console.log(data.forecast);
+        messageOne.textContent = data.location;
+        messageTwo.textContent = data.forecast;
       }
     });
   });
