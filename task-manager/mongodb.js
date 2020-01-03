0db0cfffa902491c7d32100b5ec8f2ab4875093b
Akshay Kumar <akthecoder@gmail.com>
2020-01-03 18:38:19 +0530

    Lec[76]: Inserting Documents

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -9,12 +9,46 @@ MongoClient.connect(connectionURL, {useNewUrlParser: true, useUnifiedTopology: t
     return console.log('Unable to connect to DB.');
   }
   const db = client.db(databaseName);
-  db.collection('users').insertOne({
-    name: 'Akshay',
-    age: 25
-  }, (error, result) => {
+  // db.collection('users').insertOne({
+  //   name: 'Akshay',
+  //   age: 25
+  // }, (error, result) => {
+  //   if(error) {
+  //     return console.log('Unable to insert user');
+  //   }
+  //   console.log(result.ops);
+  // });
+  // db.collection('users').insertMany([
+  //   {
+  //     name: 'Kumar',
+  //     age: 26
+  //   },
+  //   {
+  //     name: 'Kaushik',
+  //     age:24,
+  //   }
+  // ], (error, result) => {
+  //   if(error) {
+  //     return console.log('Unable to insert documents');
+  //   }
+  //   console.log(result.ops);
+  // });
+  db.collection('tasks').insertMany([
+    {
+      completed: true,
+      description: "Descriptoin of this task",
+    },
+    {
+      completed: true,
+      description: 'Eat Breakfast',
+    },
+    {
+      completed: false,
+      description: 'Clean House',
+    }
+  ], (error, result) => {
     if(error) {
-      return console.log('Unable to insert user');
+      return console.log('Unable to insert tasks');
     }
     console.log(result.ops);
   });
