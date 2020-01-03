a9efb284ee6c19a9e3470b9e74e3339537026bc0
Akshay Kumar <akthecoder@gmail.com>
2020-01-03 13:04:50 +0530

    Lec[58]: Creating a search form

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,12 +1,18 @@
-console.log("js lodaed");
-fetch('http://localhost:3000/weather?address=Noida').then((response) => {
-  response.json(response).then((data) => {
-    if(data.error) {
-      console.log(data.error);
-    }
-    else {
-      console.log(data.location);
-      console.log(data.forecast);
-    }
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+
+weatherForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const location = search.value;
+  fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    response.json(response).then((data) => {
+      if(data.error) {
+        console.log(data.error);
+      }
+      else {
+        console.log(data.location);
+        console.log(data.forecast);
+      }
+    });
   });
 });
