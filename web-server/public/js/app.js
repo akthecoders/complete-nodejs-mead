dc618c97ce704b326c43e3c56ccf9e7e2309912a
Akshay Kumar <akthecoder@gmail.com>
2020-01-03 12:57:22 +0530

    Lec[57]: Browser HTTP Request with Fetch

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,12 @@
-console.log('app');
+console.log("js lodaed");
+fetch('http://localhost:3000/weather?address=Noida').then((response) => {
+  response.json(response).then((data) => {
+    if(data.error) {
+      console.log(data.error);
+    }
+    else {
+      console.log(data.location);
+      console.log(data.forecast);
+    }
+  });
+});
