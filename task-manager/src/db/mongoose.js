f72decab96cd97cbb6ab8cb90e042018bf860810
Akshay Kumar <akthecoder@gmail.com>
2020-01-10 13:00:26 +0530

    Lec[85] : Data Validation 1

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
+
 const connectionURL = 'mongodb://127.0.0.1:27017/task-manager-api';
 
 mongoose.connect(connectionURL, {
@@ -7,41 +9,60 @@ mongoose.connect(connectionURL, {
   useUnifiedTopology: true
 });
 
-// const User = mongoose.model('User', {
-//   name: {
-//     type: String
-//   },
-//   age: {
-//     type: Number
-//   }
-// });
-
-// const me = new User({
-//   name: "Akshay Kumar",
-//   age: "Akshay"
-// });
-
-// me.save().then(() => {
-//   console.log('me', me);
-// }).catch((error) => {
-//   console.log('error', error);
-// });
-
-const Tasks = mongoose.model('Tasks', {
-  description: {
-    type: String
+const User = mongoose.model('User', {
+  name: {
+    type: String,
+    required: true,
+    trim: true,
   },
-  completed: {
-    type: Boolean
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if(!validator.isEmail(value)) {
+        throw new Error('Email is invalid');
+      }
+    }
+  },
+  age: {
+    type: Number,
+    default: 0,
+    validate(value) {
+      if(value < 0) {
+        throw new Error('Age must be a positive number');
+      }
+    }
   }
 });
 
-const task = new Tasks({
-  description: "Description of the task",
-  completed: true
+const me = new User({
+  name: "Akshay Kumar      ",
+  email: "akthecoder@gmail.com   "
 });
-task.save().then(() => {
-    console.log('task', task);
-  }).catch((error) => {
-    console.log('error', error);
-  });
+
+me.save().then(() => {
+  console.log('me', me);
+}).catch((error) => {
+  console.log('error', error);
+});
+
+// const Tasks = mongoose.model('Tasks', {
+//   description: {
+//     type: String
+//   },
+//   completed: {
+//     type: Boolean
+//   }
+// });
+
+// const task = new Tasks({
+//   description: "Description of the task",
+//   completed: true
+// });
+// task.save().then(() => {
+//     console.log('task', task);
+//   }).catch((error) => {
+//     console.log('error', error);
+//   });
