d310381d3f304d02e55f7c4a94ad906ab9290782
Akshay Kumar <akthecoder@gmail.com>
2020-01-10 15:33:27 +0530

    Lec[91]: Resource Reading enpoint 1

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -16,6 +16,26 @@ app.post('/users', (req, res) => {
   });
 });
 
+app.get('/users', (req, res) => {
+  User.find({}).then((users) => {
+    res.status(201).send(users);
+  }).catch((err) => {
+    res.status(500).send(err);
+  });
+});
+
+app.get('/users/:id', (req, res) => {
+  const _id = req.params.id;
+  User.findById(_id).then((user) => {
+    if(!user) {
+      return res.status(404).send();
+    }
+    res.status(201).send(user);
+  }).catch((err) => {
+    res.status(500).send();
+  });
+});
+
 app.post('/tasks', (req, res) => {
   const task = new Task(req.body);
   task.save().then(() => {
